477760b1f6ce1c69d2f58c79f458566b79418135
ShanRastogi22 <nash82826@gmail.com>
2023-03-30 08:47:19 +0530

    api card

--- src/Pages/View_book/View_books.js
+++ src/Pages/View_book/View_books.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
+import BookCard from '../../component/BookCard'
 import Nav from '../../component/Nav'
 const View_books = () => {
+
+  const [ data, setData ] = useState([]);
+  console.log(data)
+ 
+  const viewAllBooks = async () => {
+    await axios.get('http://localhost:8000/save-book-data').then((res) => {
+      if(res) {
+        setData(res.data);
+      }
+    })
+  }
+
+useEffect(() => {
+  viewAllBooks();
+},[])
   return (
     <div>
       <Nav />
-     <h1> view_books</h1>
+     <BookCard />
     </div>
   )
 }
